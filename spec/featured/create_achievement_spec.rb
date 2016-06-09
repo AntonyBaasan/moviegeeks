69239f9e6c2496807106e59291c143f899d4a1cd
Ankhbayar Baasandorj <antony.baasan@gmail.com>
2016-06-09 13:58:40 -0600

    add codeclimate

---
+++ spec/featured/create_achievement_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'Create new achievement' do
+
+  scenario 'create new achievement with valid data' do
+
+    visit("/")
+    click_on("New Achievement") # button or link, alternatively could use ID or Class
+
+
+    fill_in('Title', with: 'Read a book')
+    fill_in('Description', with: 'Excellent read')
+    select('Public', from: 'Privacy')
+    check('Featured achievement')
+    attach_file('Cover Image', "#{Rails.root}/spec/fixtures/cover_image.png")
+    click_on("Create Achievement")
+
+    expect(page).to have_content('Achievement has been created')
+    expect(Achievement.last.title).to eq('Read a book')
+  end
+end
