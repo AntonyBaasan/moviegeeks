00d1964ae9a23df67352fed9dfb853d36b634ff7
Ankhbayar Baasandorj <antony.baasan@gmail.com>
2016-06-18 17:14:55 -0600

    fix typo

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -2,18 +2,105 @@ require 'rails_helper'
 
 RSpec.describe AchievementsController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'Guest user' do
+    describe 'GET #index' do
+      it 'returns http success' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'renders :index template' do
+        get :index
+        expect(response).to render_template(:index)
+      end
+
+      it 'assigns only public achievement to template' do
+        public_achievement = FactoryGirl.create(:public_achievement)
+        private_achievement = FactoryGirl.create(:private_achievement)
+
+        get :index
+        expect(assigns(:achievements)).to match_array([public_achievement])
+      end
+    end
+
+    describe 'GET #show' do
+      let(:achievement) { FactoryGirl.create(:public_achievement) }
+
+      it 'returns http success' do
+        get :show, params: {id: achievement.id}
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'renders :show template' do
+        get :show, params: {id: achievement.id}
+        expect(response).to render_template(:show)
+      end
+
+      it 'assigns requested achievement to @achievement' do
+        get :show, params: {id: achievement.id} # could pass as object
+        expect(assigns(:achievement)).to eq(achievement)
+      end
+    end
+
+    describe 'GET #new' do
+      it 'redirect to login page' do
+        get :new
+        expect(response).to redirect_to(new_user_session_url)
+      end
+    end
+
+    describe 'POST #create' do
+      let(:achievement_hash) { FactoryGirl.attributes_for(:achievement) }
+
+      it 'redirect to login page' do
+        post :create, params: {achievement: achievement_hash}
+        expect(response).to redirect_to(new_user_session_url)
+      end
+    end
+
+    describe 'GET #edit' do
+      let(:achievement_hash) { FactoryGirl.attributes_for(:achievement) }
+
+      it 'redirect to login page' do
+        post :create, params: {achievement: achievement_hash}
+        expect(response).to redirect_to(new_user_session_url)
+      end
+    end
+
+    describe 'PUT #update' do
+      let(:achievement_hash) { FactoryGirl.attributes_for(:achievement) }
+
+      it 'redirect to login page' do
+        put :update, params: {id: FactoryGirl.create(:public_achievement), achievement: achievement_hash}
+        expect(response).to redirect_to(new_user_session_url)
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      it 'redirect to login page' do
+        delete :destroy, params: {id: FactoryGirl.create(:public_achievement)}
+        expect(response).to redirect_to(new_user_session_url)
+      end
+    end
+
+  end
+
+  describe 'Authenticated user' do
+
+  end
+
+  describe 'GET #index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
 
-    it "renders :index template" do
+    it 'renders :index template' do
       get :index
       expect(response).to render_template(:index)
     end
 
-    it "assigns only public achievement to template" do
+    it 'assigns only public achievement to template' do
       public_achievement = FactoryGirl.create(:public_achievement)
       private_achievement = FactoryGirl.create(:private_achievement)
 
@@ -22,122 +109,122 @@ RSpec.describe AchievementsController, type: :controller do
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
+  describe 'GET #new' do
+    it 'returns http success' do
       get :new
       expect(response).to have_http_status(:success)
     end
 
-    it "renders :new template" do
+    it 'renders :new template' do
       get :new
       expect(response).to render_template(:new)
     end
 
-    it "assigns new achievement to @achievement" do
+    it 'assigns new achievement to @achievement' do
       get :new
       expect(assigns(:achievement)).to be_a_new(Achievement)
     end
   end
 
-  describe "GET #edit" do
+  describe 'GET #edit' do
     let(:achievement) { FactoryGirl.create(:public_achievement) }
 
-    it "returns http success" do
+    it 'returns http success' do
       get :edit, params: {id: achievement.id}
       expect(response).to have_http_status(:success)
     end
 
-    it "renders :edit template" do
+    it 'renders :edit template' do
       get :edit, params: {id: achievement.id}
       expect(response).to render_template(:edit)
     end
 
-    it "assigns passed achievement to @achievement" do
+    it 'assigns passed achievement to @achievement' do
       get :edit, params: {id: achievement.id} # could pass as object
       expect(assigns(:achievement)).to eq(achievement)
     end
   end
 
-  describe "PUT #edit" do
+  describe 'PUT #edit' do
     let(:achievement) { FactoryGirl.create(:public_achievement) }
 
-    context "valid data" do
-      let(:valid_data) { FactoryGirl.attributes_for(:achievement, title: "New Title") }
+    context 'valid data' do
+      let(:valid_data) { FactoryGirl.attributes_for(:achievement, title: 'New Title') }
 
-      it "redirects to achievements#show" do
+      it 'redirects to achievements#show' do
         put :update, params: {id: achievement.id, achievement: valid_data}
         expect(response).to redirect_to(achievement)
       end
-      it "updates the achievement in database" do
+      it 'updates the achievement in database' do
         put :update, params: {id: achievement.id, achievement: valid_data}
         achievement.reload
-        expect(achievement.title).to eq("New Title")
+        expect(achievement.title).to eq('New Title')
       end
     end
 
-    context "invalid data" do
-      let(:invalid_data) { FactoryGirl.attributes_for(:achievement, title: "") }
+    context 'invalid data' do
+      let(:invalid_data) { FactoryGirl.attributes_for(:achievement, title: '') }
 
-      it "renders #edit template" do
+      it 'renders #edit template' do
         put :update, params: {id: achievement.id, achievement: invalid_data}
         expect(response).to render_template(:edit)
       end
-      it "do not updates the achievement in database" do
+      it 'do not updates the achievement in database' do
         put :update, params: {id: achievement.id, achievement: invalid_data}
         achievement.reload
-        expect(achievement.title).not_to eq("New Title")
+        expect(achievement.title).not_to eq('New Title')
       end
 
     end
 
   end
 
-  describe "GET #show" do
+  describe 'GET #show' do
     let(:achievement) { FactoryGirl.create(:public_achievement) }
 
-    it "returns http success" do
+    it 'returns http success' do
       get :show, params: {id: achievement.id}
       expect(response).to have_http_status(:success)
     end
 
-    it "renders :show template" do
+    it 'renders :show template' do
       get :show, params: {id: achievement.id}
       expect(response).to render_template(:show)
     end
 
-    it "assigns requested achievement to @achievement" do
+    it 'assigns requested achievement to @achievement' do
       get :show, params: {id: achievement.id} # could pass as object
       expect(assigns(:achievement)).to eq(achievement)
     end
   end
 
-  describe "POST #create" do
-    context "valid data" do
+  describe 'POST #create' do
+    context 'valid data' do
       # creates hash of attributes and values
       let(:achievement_hash) { FactoryGirl.attributes_for(:achievement) }
 
-      it "redirects to achievements#show view" do
+      it 'redirects to achievements#show view' do
         post :create, params: {achievement: achievement_hash}
         expect(response).to redirect_to(achievement_path(assigns(:achievement)))
       end
 
-      it "creates new achievements in db" do
+      it 'creates new achievements in db' do
         expect {
           post :create, params: {achievement: achievement_hash}
         }.to change(Achievement, :count).by(1)
       end
     end
 
-    context "invalid data" do
+    context 'invalid data' do
       # creates hash of attributes and values
       let(:invalid_achievement_hash) { FactoryGirl.attributes_for(:achievement, title: '') }
 
-      it "renders #new view" do
+      it 'renders #new view' do
         post :create, params: {achievement: invalid_achievement_hash}
         expect(response).to render_template(:new)
       end
 
-      it "do not add new achievements in db" do
+      it 'do not add new achievements in db' do
         expect {
           post :create, params: {achievement: invalid_achievement_hash}
         }.to change(Achievement, :count).by(0)
@@ -146,19 +233,19 @@ RSpec.describe AchievementsController, type: :controller do
 
   end
 
-  describe "DELETE #destroy" do
-      let(:achievement) { FactoryGirl.create(:achievement) }
+  describe 'DELETE #destroy' do
+    let(:achievement) { FactoryGirl.create(:achievement) }
 
-      it "redirects to achievements#index view" do
-        delete :destroy, params: {id: achievement.id}
-        expect(response).to redirect_to(achievement_path)
-      end
+    it 'redirects to achievements#index view' do
+      delete :destroy, params: {id: achievement.id}
+      expect(response).to redirect_to(achievement_path)
+    end
 
-      it "deletes an achievements from database" do
-        delete :destroy, params: {id: achievement.id}
+    it 'deletes an achievements from database' do
+      delete :destroy, params: {id: achievement.id}
 
-        expect(Achievement.exists?(achievement.id)).to be_falsy
-      end
+      expect(Achievement.exists?(achievement.id)).to be_falsy
+    end
 
   end
 
